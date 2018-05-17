eebee734387d62b5ba2bfc75ce99f89debe7c0c0
Daniel Ivanovich <Soccer.Ivanovich@gmail.com>
2018-05-17 08:29:23 -0400

    Finally fixed video loading

--- js/trailer.js
+++ js/trailer.js
@@ -5,22 +5,21 @@ $('#trailer').one('canplay', function (event) {
 
     console.log("Video loaded");
 
-    show();
+    play();
 });
 
 let videoLoaded = setInterval(() =>{
     if(document.getElementById('trailer').readyState == 4){
         console.log("Video loaded");
 
-        show();
+        play();
 
         clearInterval(videoLoaded);
     }
 }, 500);
 
-let show = function() {
-
-    document.getElementById('trailer-container').style.opacity = "1";
-
+let play = function() {
     document.getElementById('section-1').style.background = "none";
+    document.getElementById('trailer').play();
+    document.getElementById('music').play();
 };
