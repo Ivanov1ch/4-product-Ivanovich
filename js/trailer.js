9d7e2335c03d524d1e55b2b60d5ed89ef723b639
Daniel Ivanovich <Soccer.Ivanovich@gmail.com>
2018-05-17 08:52:07 -0400

    Attempting to avoid chrome autoplay restrictions
    Only partly worked

--- js/trailer.js
+++ js/trailer.js
@@ -1,3 +1,7 @@
+$( document ).ready(() => {
+    intervalManager(true, 500);
+});
+
 $('#trailer').one('loadstart', function (event) {
     console.log("Video started loading");
 });
@@ -8,18 +12,46 @@ $('#trailer').one('canplay', function (event) {
     play();
 });
 
-let videoLoaded = setInterval(() =>{
-    if(document.getElementById('trailer').readyState == 4){
-        console.log("Video loaded");
+var videoLoaded = null;
 
-        play();
+let intervalManager = function (flag, time) {
+    if(flag)
+        videoLoaded =  setInterval(() =>{
+            if(document.getElementById('trailer').readyState == 4){
+                console.log("Video loaded");
 
+                play();
+
+                clearInterval(videoLoaded);
+            }
+        }, time);
+    else
         clearInterval(videoLoaded);
-    }
-}, 500);
+}
+
 
 let play = function() {
     document.getElementById('section-1').style.background = "none";
     document.getElementById('trailer').play();
     document.getElementById('music').play();
 };
+
+//Dealing with the required user interaction - On any interaction, attempt the reload
+$('#body').one('scroll', () => {
+    if(!isPlaying('video')){
+        document.getElementById('trailer').pause();
+        document.getElementById('music').pause();
+        intervalManager(true, 500);
+    }
+});
+$('#body').one('click', ()=>{
+    if(!isPlaying('video')){
+        document.getElementById('trailer').pause();
+        document.getElementById('music').pause();
+        intervalManager(true, 500);
+    }
+});
+
+let isPlaying = function(id){
+    return document.getElementById(id).currentTime > 0 && !document.getElementById(id).paused && !document.getElementById(id).ended
+}
