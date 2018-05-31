f02e09d6fd53941cefbb90f90cea680ef626c97e
PlantsVsDan <Soccer.Ivanovich@gmail.com>
2018-05-31 17:25:18 -0400

    Mute audio on modal open
    Want them to be able to hear the video

--- js/modal.js
+++ js/modal.js
@@ -1,22 +1,27 @@
 // Get the modal
-var modal = document.getElementById('myModal');
+let modal = document.getElementById('myModal');
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+let span = document.getElementsByClassName("close")[0];
+
+let audio = document.getElementById('music');
 
 // When the user clicks on the button, open the modal 
 function showModal() {
     modal.style.display = "block";
+    audio.pause();
 }
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
     modal.style.display = "none";
+    audio.play();
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
+        audio.play();
     }
 }
